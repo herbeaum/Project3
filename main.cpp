9bdebf31d5e5507c403308e2de8626b24a6493f5
Mickael <mickael.herbeaux@gmail.com>
2016-11-26 15:53:39 +0100

    comment main

--- main.cpp
+++ main.cpp
@@ -15,14 +15,23 @@ using namespace std;
 #include "Point.h"
 
 int main(int argc, char** argv) {
+    /*
+     *Creation of the domain
+     */
     Curve c1(-10,5,1);
     Line L2(true, 5,0,3,1);
     Line L3(false,3,-10,5,1);
     Line L4(true,-10,0,3,1);
     Domain d(c1,L2,L3,L4);
+    /*
+     *Creation of the grid
+     */
     int m=50;
     int n=20;
     d.generating_grid(m,n);
+    /*
+     *Save the grid in binary file
+     */
     d.save();
     return 0;
 }
